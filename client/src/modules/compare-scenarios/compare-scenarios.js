7413cf7e2d5f6582f1e3a04340a76d187122250b
Brian Park <park-brian@users.noreply.github.com>
2022-10-17 00:36:04 -0400

    add compare-scenarios module

--- client/src/modules/compare-scenarios/compare-scenarios.js
+++ client/src/modules/compare-scenarios/compare-scenarios.js
@@ -1,24 +1,92 @@
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import { useRecoilState, useResetRecoilState } from "recoil";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import { scenariosState } from "./state";
+import { readFile } from "./utils";
 
 export default function CompareScenarios() {
+  const [scenarios, setScenarios] = useRecoilState(scenariosState);
+
+  async function addScenario(event) {
+    const files = event.target.files;
+
+    if (files.length > 0) {
+      try {
+        const file = files[0];
+        let scenario = JSON.parse(await readFile(file));
+        scenario.name = file.name;
+        setScenarios((scenarios) => [...scenarios, scenario]);
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+
+    event.target.value = "";
+  }
+
+  function renameScenario(index, name) {
+    console.log({index, name})
+    setScenarios((scenarios) => {
+      let newScenarios = scenarios.map(s => ({...s}));
+      newScenarios[index].name = name;
+      return newScenarios;
+    });
+  }
+
+  function removeScenario(index) {
+    setScenarios((scenarios) => scenarios.filter((_, i) => i !== index));
+  }
+
   return (
-    <div className="bg-light py-5">
+    <div className="bg-light py-4">
       <Container>
-        <Row>
-          <Col>
-            <article className="shadow-lg p-4 rounded">
-              <h1 className="font-title text-muted h3 mb-4">Run Models</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sodales odio ac neque gravida mattis. Nulla facilisi. Proin velit purus, lacinia eu lacinia a, ultricies vitae urna. Quisque congue semper libero, ac blandit ipsum iaculis vitae. Suspendisse potenti. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin ac semper massa, a bibendum magna. Vestibulum dictum orci mauris, at rhoncus nisi vehicula at. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sem nisl, egestas at sagittis eu, imperdiet vel magna.</p>
-                <p>Vestibulum nec facilisis ex. Fusce semper, nisl ut vulputate aliquet, felis arcu viverra dolor, vel efficitur erat purus id diam. Vestibulum libero nisi, ultrices ut lacus eu, blandit venenatis ex. Fusce posuere libero dignissim orci lobortis, quis malesuada erat iaculis. Sed euismod ultricies nulla id porta. Cras rhoncus massa eget libero lacinia commodo. Aenean vestibulum mi id neque viverra condimentum. Aenean venenatis nec ligula a pulvinar. Sed eget vestibulum augue. Aliquam a mauris sit amet enim imperdiet imperdiet a non nulla. Pellentesque et gravida dolor. Nulla rhoncus viverra mattis.</p>
-                <p>Mauris sed diam consectetur, suscipit turpis ac, lacinia tortor. Suspendisse mollis vel neque sit amet sodales. Curabitur magna ligula, iaculis ac sagittis at, tincidunt imperdiet augue. Vivamus ut diam nunc. Vestibulum et nisi a felis tempor rutrum quis ut dui. Duis finibus non mi in ullamcorper. Ut vulputate enim ut enim sollicitudin, sed pharetra ex rutrum. In dapibus condimentum dolor bibendum interdum. Integer nibh mauris, dapibus sit amet tincidunt sed, semper at diam. Mauris ornare erat magna, non hendrerit erat pellentesque vitae.</p>
-                <p>Curabitur pharetra ultricies diam eget auctor. Integer porta dignissim lacus, volutpat bibendum nulla luctus id. Aliquam venenatis purus et purus vehicula, sed euismod tellus pharetra. Fusce interdum blandit vehicula. Integer quis ante eget mauris volutpat bibendum vel id eros. Cras sed dignissim lorem. Proin porta urna justo, vitae venenatis arcu viverra hendrerit. Sed vulputate posuere feugiat. Aliquam fringilla eros ut suscipit faucibus. Vivamus rutrum lorem in ipsum semper dapibus ac non lacus. Fusce congue enim ut vestibulum faucibus. Fusce malesuada malesuada felis ut posuere. Ut sed nunc auctor, pulvinar nibh at, interdum ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p>Mauris porta tellus nulla, vitae posuere lorem vulputate ac. Phasellus eget tristique nunc, in maximus velit. Mauris ac erat posuere, vulputate neque quis, tristique metus. Vestibulum at leo leo. Vestibulum vulputate at ex non iaculis. Nulla fermentum dolor at nulla aliquet, id euismod ligula aliquet. Fusce vel ex dictum, placerat quam sit amet, semper ligula. Phasellus quis iaculis mauris. Aliquam porttitor risus eu libero pretium fermentum. Nullam fermentum arcu in elementum ultrices. Quisque posuere, magna et mattis lobortis, eros dui aliquet ante, vel aliquet mi nibh in ligula.</p>
-            </article>
-          </Col>
-        </Row>
+        <Form>
+          <Card className="mb-4">
+            <Card.Header>
+              <Card.Title>Compare Scenarios</Card.Title>
+              <Card.Text className="small text-muted">Please upload scenarios for comparison.</Card.Text>
+            </Card.Header>
+            <Card.Body className="p-0">
+              <ListGroup variant="flush hover striped">
+                {scenarios.map((scenario, index) => (
+
+                  <ListGroup.Item key={index}>
+                    <Form.Group as={Row} controlId="populationSize">
+                      <Form.Label column md={2}>
+                        Scenario {index + 1}
+                      </Form.Label>
+                      <Col md={8}>
+                        <Form.Control
+                          placeholder="Enter name"
+                          className="transparent mb-md-0 mb-2 px-0"
+                          value={scenario.name}
+                          onChange={ev => renameScenario(index, ev.target.value)}
+                        />
+                      </Col>
+                      <Col md={2} className="text-md-end">
+                        <Button variant="danger" onClick={ev => removeScenario(index)}>Remove</Button>                  
+                      </Col>
+                    </Form.Group>
+                  </ListGroup.Item>
+                ))}
+                <ListGroup.Item >
+                  <label htmlFor="fileInput" className="btn btn-outline-primary">
+                    Add Scenario
+                  </label>
+                  <input id="fileInput" type="file" onChange={addScenario} hidden />
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Form>
       </Container>
     </div>
-  )
+  );
 }
---
+++ client/src/modules/compare-scenarios/state.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const scenariosState = atom({
+  key: "compareScenarios.scenariosState",
+  default: [],
+});
