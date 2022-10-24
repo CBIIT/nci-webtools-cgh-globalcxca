c2d01aac463e90dcf25dc1280b0acd8b28bd1a4f
Brian Park <park-brian@users.noreply.github.com>
2022-10-24 13:30:17 -0400

    remove unused import

--- client/src/modules/about/about.js
+++ client/src/modules/about/about.js
@@ -1,6 +1,4 @@
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
 import Card from "react-bootstrap/Card"
 
 export default function About() {
