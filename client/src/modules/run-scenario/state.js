86b10f1a796ada9274455589f935638cb7bf17e1
Brian Park <park-brian@users.noreply.github.com>
2022-10-26 01:59:27 -0400

    provide additional state

--- client/src/modules/run-scenario/state.js
+++ client/src/modules/run-scenario/state.js
@@ -3,28 +3,35 @@ import { defaultParameters } from "./models";
 
 const debug = false;
 
+export const defaultFormState = {
+	scenario: "ScreenDiagnosticTestTreat",
+	populationSize: "",
+	hpvCancerPrevalence: "",
+	hpvPrevalence: "",
+	cinPrevalence: "",
+	screeningInterval: "",
+	percentScreened: "",
+	percentTriaged: "",
+	percentDiagnosticTriaged: "",
+	percentTreated: "",
+	screeningTest: "",
+	screeningTestSensitivity: "",
+	screeningTestSpecificity: "",
+	triageTest: "",
+	triageTestSensitivity: "",
+	triageTestSpecificity: "",
+	diagnosticTest: "",
+	diagnosticTestSensitivity: "",
+	diagnosticTestSpecificity: "",
+	...(debug && defaultParameters)
+}
+
 export const formState = atom({
 	key: "runScenarios.formState",
-	default: {
-		scenario: "ScreenDiagnosticTestTreat",
-		populationSize: "",
-		hpvCancerPrevalence: "",
-		hpvPrevalence: "",
-		cinPrevalence: "",
-		screeningInterval: "",
-		percentScreened: "",
-		percentTriaged: "",
-		percentDiagnosticTriaged: "",
-		percentTreated: "",
-		screeningTest: "",
-		screeningTestSensitivity: "",
-		screeningTestSpecificity: "",
-		triageTest: "",
-		triageTestSensitivity: "",
-		triageTestSpecificity: "",
-		diagnosticTest: "",
-		diagnosticTestSensitivity: "",
-		diagnosticTestSpecificity: "",
-		...(debug && defaultParameters)
-	}
+	default: defaultFormState
+});
+
+export const resultsState = atom({
+	key: "runScenarios.resultsState",
+	default: null
 });
