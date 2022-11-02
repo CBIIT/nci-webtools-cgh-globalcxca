9d883bedb80778e8a690ecf85eb31a59ebf78f4e
Brian Park <park-brian@users.noreply.github.com>
2022-11-02 13:43:41 -0400

    add locale change listener to app

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
 import { Outlet } from "react-router-dom";
 import Navbar from './modules/navbar/navbar';
 import { routes } from './routes';
+import { localeState } from './app.state';
 
 export default function App() {
   const childRoutes = routes[0].children;
+  const setLocale = useSetRecoilState(localeState);
+
+  useEffect(() => window.registerLocaleChangeListener(setLocale), [setLocale]);
 
   return (
     <>
---
+++ client/src/app.state.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const localeState = atom({
+  key: "localeState",
+  default: "en",
+});
