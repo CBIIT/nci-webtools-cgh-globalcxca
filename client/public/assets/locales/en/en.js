a83358f2588aa836a665393386e9a38d19b19c12
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-05-18 16:50:04 -0400

    add locale en

---
+++ client/public/assets/locales/en/en.js
@@ -0,0 +1,29 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+
+//import locale file
+import enLocale from "./en.json";
+
+// Initialize i18next
+i18next
+  .use(initReactI18next) // Initialize i18next with react-i18next
+  .init({
+    lng: "en", // Set the default language
+    fallbackLng: "en", // Fallback language if translation is not available
+    debug: true, // Enable debug mode for development
+
+    // Add your translation resources
+    resources: {
+      en: {
+        translation: enLocale, // English locale file
+      },
+      // Add other language translations here if needed
+    },
+
+    // Common configuration options
+    interpolation: {
+      escapeValue: false, // By default, react-i18next escapes special characters
+    },
+  });
+
+export default i18next;
