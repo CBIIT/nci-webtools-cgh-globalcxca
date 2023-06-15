8fd9939a756d171bff42305077b627bc2c8d9e89
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-06-15 15:46:56 -0400

    build project to update the scenario comparison

--- client/src/services/plot-utils.js
+++ client/src/services/plot-utils.js
@@ -1,14 +1,13 @@
 export function exportSvg(selector, filename) {
-    const svgString = new XMLSerializer().serializeToString(
-      document.querySelector(selector)
-    );
-    const svgBlob = new Blob([svgString], { type: 'image/svg+xml' });
-    const svgUrl = URL.createObjectURL(svgBlob);
-    const downloadLink = document.createElement('a');
-    downloadLink.href = svgUrl;
-    downloadLink.download = filename;
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-  }
-  
+  const svgString = new XMLSerializer().serializeToString(
+    document.querySelector(selector)
+  );
+  const svgBlob = new Blob([svgString], { type: "image/svg+xml" });
+  const svgUrl = URL.createObjectURL(svgBlob);
+  const downloadLink = document.createElement("a");
+  downloadLink.href = svgUrl;
+  downloadLink.download = filename;
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
