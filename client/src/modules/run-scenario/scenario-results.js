7f6cd5062ac5475b047c7559d30159e7f8718537
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-06-16 15:59:29 -0400

    put the export excel function to a seperate file

--- client/src/modules/run-scenario/scenario-results.js
+++ client/src/modules/run-scenario/scenario-results.js
@@ -102,93 +102,9 @@ export default function ScenarioResults() {
   }
 
   function exportResultsExcel() {
-    // Create a new workbook
-    const wb = XLSX.utils.book_new();
-
-    // Get the HTML elements containing the data to export
-    const cards = document.querySelectorAll(".card[data-export]");
-
-    console.log("cards", cards);
-
-    // Iterate over the cards and create a worksheet for each card
-    cards.forEach((card, index) => {
-      // Get the tables within the card
-      const tables = card.querySelectorAll("table");
-      console.log("tables", tables);
-
-      // Create an array to store the table data
-      const cardData = [];
-
-      // Iterate over the tables and extract the table data
-      tables.forEach((table) => {
-        const tableData = [];
-        const headers = [];
-        const rows = table.querySelectorAll("tr");
-
-        rows.forEach((row) => {
-          const rowData = [];
-          const cells = row.querySelectorAll("th, td");
-          cells.forEach((cell) => {
-            rowData.push(cell.innerText);
-          });
-          // Check if it's the header row
-          if (row.closest("thead")) {
-            headers.push(rowData); // Store the header row separately
-          }
-          tableData.push(rowData);
-        });
-        // headers.push(tableData[0][0]);
-
-        // Store the table data
-        cardData.push({
-          //tableName: table.getAttribute("data-table-name"),
-          tableName: headers[0][0],
-          headers: headers[0],
-          data: tableData, // Exclude the header row,
-        });
-      });
-
-      console.log("cardData", cardData);
-      // Extract the data from each table and concatenate into a single array
-      const combinedData = cardData.reduce((acc, curr) => {
-        return acc.concat(curr.data);
-      }, []);
-
-      console.log("combinedData, ", combinedData);
-
-      // Create a worksheet from the card data
-      const ws = XLSX.utils.aoa_to_sheet(combinedData);
-
-      // Set the worksheet name
-      // const sheetName = `Tab ${index + 1}`;
-      const sheetName = index === 0 ? "Input" : "Result";
-      XLSX.utils.book_append_sheet(wb, ws, sheetName);
-    });
-
-    // Generate the Excel file
-    const excelFile = XLSX.write(wb, { type: "binary", bookType: "xlsx" });
-
-    // Save the file or do whatever you want with it
-    // For example, you can create a download link
-    const blob = new Blob([s2ab(excelFile)], {
-      type: "application/octet-stream",
-    });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
     const filename = `${params.scenario} ${getTimestamp()}.xlsx`;
-    link.href = url;
-    link.download = filename;
-    link.click();
+    exportExcel(filename);
   }
-
-  // Utility function to convert string to ArrayBuffer
-  function s2ab(s) {
-    const buf = new ArrayBuffer(s.length);
-    const view = new Uint8Array(buf);
-    for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
-    return buf;
-  }
-
   //export chart to SVG
   function handleExportSvg(id) {
     const plotSelector = `#${id}`;
--- client/src/services/excel-utils.js
+++ client/src/services/excel-utils.js
@@ -1,37 +1,83 @@
 import * as XLSX from "xlsx";
-import { asLabel, asPercent } from "../services/formatters";
-import { getTimestamp } from "../services/file-utils";
 
-export function exportExcel(params, results) {
-  const filename = `${params.scenario} ${getTimestamp()}.xlsx`;
-  const workbook = XLSX.utils.book_new();
-  const inputData = XLSX.utils.aoa_to_sheet([
-    [params.scenario],
-    [
-      "Number of people in target population for cervical screening",
-      params.populationSize ?? "N/A",
-    ],
-    [
-      "Prevalence of CIN2/3 in population for cervical screening",
-      asPercent(params.cinPrevalence, 0) ?? "N/A",
-    ],
-    ["Participation in Health Services", params.screeningInterval ?? "N/A"],
-  ]);
-  XLSX.utils.book_append_sheet(workbook, inputData, "Input Data");
+export function exportExcel(filename) {
+  // Create a new workbook
+  const wb = XLSX.utils.book_new();
 
-  const resultWorksheet = XLSX.utils.aoa_to_sheet([
-    ["Annual Targets"],
-    [
-      "Population without precancer targeted for screening",
-      results.testedFalsePositives[0],
-    ],
-    [
-      "Population with precancer targeted for screening",
-      results.testedTruePositives[0],
-    ],
-    ["Third Metric", "N/A"],
-  ]);
+  // Get the HTML elements containing the data to export
+  const cards = document.querySelectorAll(".card[data-export]");
 
-  XLSX.utils.book_append_sheet(workbook, resultWorksheet, "Results");
-  XLSX.writeFile(workbook, filename);
+  // Iterate over the cards and create a worksheet for each card
+  cards.forEach((card, index) => {
+    // Get the tables within the card
+    const tables = card.querySelectorAll("table");
+
+    // Create an array to store the table data
+    const cardData = [];
+
+    // Iterate over the tables and extract the table data
+    tables.forEach((table) => {
+      const tableData = [];
+      const headers = [];
+      const rows = table.querySelectorAll("tr");
+
+      rows.forEach((row) => {
+        const rowData = [];
+        const cells = row.querySelectorAll("th, td");
+        cells.forEach((cell) => {
+          rowData.push(cell.innerText);
+        });
+        // Check if it's the header row
+        if (row.closest("thead")) {
+          headers.push(rowData); // Store the header row separately
+        }
+        tableData.push(rowData);
+      });
+      // headers.push(tableData[0][0]);
+
+      // Store the table data
+      cardData.push({
+        //tableName: table.getAttribute("data-table-name"),
+        tableName: headers[0][0],
+        headers: headers[0],
+        data: tableData, // Exclude the header row,
+      });
+    });
+
+    // Extract the data from each table and concatenate into a single array
+    const combinedData = cardData.reduce((acc, curr) => {
+      return acc.concat(curr.data);
+    }, []);
+
+    // Create a worksheet from the card data
+    const ws = XLSX.utils.aoa_to_sheet(combinedData);
+
+    // Set the worksheet name
+    // const sheetName = `Tab ${index + 1}`;
+    const sheetName = index === 0 ? "Input" : "Result";
+    XLSX.utils.book_append_sheet(wb, ws, sheetName);
+  });
+
+  // Generate the Excel file
+  const excelFile = XLSX.write(wb, { type: "binary", bookType: "xlsx" });
+
+  // Save the file or do whatever you want with it
+  // For example, you can create a download link
+  const blob = new Blob([s2ab(excelFile)], {
+    type: "application/octet-stream",
+  });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  //const filename = `${params.scenario} ${getTimestamp()}.xlsx`;
+  link.href = url;
+  link.download = filename;
+  link.click();
+}
+
+// Utility function to convert string to ArrayBuffer
+function s2ab(s) {
+  const buf = new ArrayBuffer(s.length);
+  const view = new Uint8Array(buf);
+  for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
+  return buf;
 }
