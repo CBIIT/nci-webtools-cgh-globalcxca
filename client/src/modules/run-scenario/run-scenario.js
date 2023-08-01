3c76aceca002d057685ff8d027963869d6a2f0f2
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-08-01 16:38:06 -0400

    build to fix the homepage heigh and scrollbar

--- client/src/modules/run-scenario/run-scenario.js
+++ client/src/modules/run-scenario/run-scenario.js
@@ -310,7 +310,7 @@ export default function RunScenarios() {
                                   required
                                 />
                                 <span className="text-nowrap">
-                                  {form.screeningInterval} Year(s)
+                                  {form.screeningInterval} {t("general.years")}
                                 </span>
                               </InputGroup>
                             </Col>
