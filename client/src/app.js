6efaa2fe0130445edc59f4782fb8f701a432554f
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-08-02 11:54:08 -0400

    add i18next to footet

--- client/src/app.js
+++ client/src/app.js
@@ -4,12 +4,26 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./modules/navbar/navbar";
 import { routes } from "./routes";
 import { localeState } from "./app.state";
+import { useTranslation } from "react-i18next";
 
 export default function App() {
   const childRoutes = routes[0].children;
   const setLocale = useSetRecoilState(localeState);
+  const { t, i18n } = useTranslation();
 
-  useEffect(() => window.registerLocaleChangeListener(setLocale), [setLocale]);
+  // useEffect(() => window.registerLocaleChangeListener(setLocale), [setLocale]);
+
+  useEffect(() => {
+    const unregisterLocaleChangeListener = window.registerLocaleChangeListener(
+      (locale) => {
+        i18n.changeLanguage(locale);
+      }
+    );
+
+    return () => {
+      unregisterLocaleChangeListener();
+    };
+  }, [i18n]);
 
   return (
     <>
