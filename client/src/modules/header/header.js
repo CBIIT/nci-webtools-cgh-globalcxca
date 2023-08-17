58c4276021ff8bd73eb8262896b4bc6b2c8bb089
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-08-17 00:30:05 -0400

    add the title into react so that it can change language

--- client/src/modules/header/header.js
+++ client/src/modules/header/header.js
@@ -6,32 +6,13 @@ function Header() {
   const { t } = useTranslation();
 
   return (
-    <header>
-      <div className="container mt-md-2 mb-2 d-flex align-items-center justify-content-between">
-        <a
-          className="d-inline-block"
-          href="https://www.cancer.gov/about-nci/organization/cgh"
-          target="_blank"
-        >
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/images/nci-cgh-logo.svg`}
-            height="55"
-            alt="National Cancer Institute Logo"
-            className="mw-100"
-          />
-        </a>
-        <div className="col-auto d-flex align-items-center">
-          <div id="navbarSearch">{/* Your NavbarSearch component here */}</div>
-          <div id="languageSelectContainer" className="p-3"></div>
-        </div>
-      </div>
-
+    <div>
       <div className="bg-primary text-white py-1 d-none d-lg-block">
         <div className="container">
           <h1 className="h6 fw-semibold">{t("header.title")}</h1>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
